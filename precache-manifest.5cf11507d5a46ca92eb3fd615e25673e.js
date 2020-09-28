7d01d24445a0586228ad7fa8b94a9cd82539dfe0
Nipun Goyal <nipungoyal96@gmail.com>
2020-09-28 22:09:58 +0530

    Updates

---
+++ precache-manifest.5cf11507d5a46ca92eb3fd615e25673e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3e6555025be43645ee6de2647d6abf2",
+    "url": "/trelloClone/index.html"
+  },
+  {
+    "revision": "88f98e9d6133b3ccba62",
+    "url": "/trelloClone/static/css/main.bd30988f.chunk.css"
+  },
+  {
+    "revision": "5c7d16b096a0843c0888",
+    "url": "/trelloClone/static/js/2.e74125f1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/trelloClone/static/js/2.e74125f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88f98e9d6133b3ccba62",
+    "url": "/trelloClone/static/js/main.94a3410e.chunk.js"
+  },
+  {
+    "revision": "ce0c28f0bdae8c4c3887",
+    "url": "/trelloClone/static/js/runtime-main.0fe6c42e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ea118109ba5b6bd50e318dc87d5821c2.js"
+  "/trelloClone/precache-manifest.5cf11507d5a46ca92eb3fd615e25673e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/trelloClone/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
